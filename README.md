d91573cfba98ddba3d96d078562db6443f324dc3
Dorian Stoll <dorian@tmsp.io>
2017-04-11 10:31:29 +0200

    Ported the adapter plugin to go

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 ## SpaceDock-Extras
 This repository contains all the code that we need for running SpaceDock, but that doesn't fit into the idea of a general purpose mod-hosting backend. It also contains the modules we just wanted to make.
-
-### Modules
-* profiler.py: Profiles the performance of all API endpoints
-* documentation.py: Generates Webdocs using Python docstrings.
---
+++ plugins/adapter/adapter.go
@@ -0,0 +1,39 @@
+/*
+ Route Adapters for SpaceDock Backend
+
+ SpaceDock-Extras is licensed under the Terms of the MIT License.
+ Copyright (c) 2017 Dorian Stoll (StollD), RockyTV
+*/
+
+package adapter
+
+import (
+    "SpaceDock"
+    "SpaceDock/routes"
+    "SpaceDock/utils"
+    "github.com/spf13/cast"
+    "gopkg.in/kataras/iris.v6"
+)
+
+func init() {
+    Register(GET, "/api/adapter/mods/:modid", mods_adapter)
+}
+
+/*
+ Path: /api/adapter/mods/:modid
+ Method: GET
+ Description: Returns information for one mod
+ */
+func mods_adapter(ctx *iris.Context) {
+    // Get params
+    modid := cast.ToUint(ctx.GetString("modid"))
+
+    // Get the mods gameshort
+    mod := &objects.Mod{}
+    SpaceDock.Database.Where("id = ?", modid).First(mod)
+    if mod.ID != modid {
+        utils.WriteJSON(ctx, iris.StatusNotFound, utils.Error("The modid is invalid").Code(2130))
+        return
+    }
+    ctx.Redirect("/api/mods/" + mod.Game.Short + "/" + cast.ToString(modid), iris.StatusPermanentRedirect)
+}
