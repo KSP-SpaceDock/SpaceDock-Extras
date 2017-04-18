0c70d1c7f1ef35d9609382ad8233a3ba265dea3a
Dorian Stoll <dorian@tmsp.io>
2017-04-18 20:43:56 +0200

    Add the first data transformer

---
+++ plugins/transformers/transformer.go
@@ -0,0 +1,26 @@
+/*
+ Data transformation extension for SpaceDock Backend
+
+ SpaceDock-Extras is licensed under the Terms of the MIT License.
+ Copyright (c) 2017 Dorian Stoll (StollD), RockyTV
+*/
+
+package transformers
+
+import (
+    "SpaceDock/objects"
+    "SpaceDock/utils"
+)
+
+package transformers
+
+func init() {
+    utils.RegisterDataTransformer(TransformMod)
+}
+
+func TransformMod(data interface{}, m map[string]interface{}) {
+    if mod, ok := data.(objects.Mod); ok {
+        m["follower_count"] = len(mod.Followers)
+        m["author"] = mod.User.Username
+    }
+}
