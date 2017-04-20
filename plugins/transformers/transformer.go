e9975f72d824822c7ad80ce25e6a5a6c865f1ab4
Dorian Stoll <dorian@tmsp.io>
2017-04-20 17:23:23 +0200

    Expose the gameshort in the modinfo

--- plugins/transformers/transformer.go
+++ plugins/transformers/transformer.go
@@ -21,21 +21,25 @@ func Transform(data interface{}, m map[string]interface{}) {
     if mod, ok := data.(*objects.Mod); ok {
         m["follower_count"] = len(mod.Followers)
         m["author"] = mod.User.Username
+        m["game_short"] = mod.Game.Short
     }
     if mod, ok := data.(objects.Mod); ok {
         m["follower_count"] = len(mod.Followers)
         m["author"] = mod.User.Username
+        m["game_short"] = mod.Game.Short
     }
     if _, ok := data.(*objects.Featured); ok {
         mod := &objects.Mod{}
         SpaceDock.Database.Where("id = ?", m["mod_id"]).First(mod)
         (m["mod"].(map[string]interface{}))["follower_count"] = len(mod.Followers)
         (m["mod"].(map[string]interface{}))["author"] = mod.User.Username
+        (m["mod"].(map[string]interface{}))["game_short"] = mod.Game.Short
     }
     if _, ok := data.(objects.Featured); ok {
         mod := &objects.Mod{}
         SpaceDock.Database.Where("id = ?", m["mod_id"]).First(mod)
         (m["mod"].(map[string]interface{}))["follower_count"] = len(mod.Followers)
         (m["mod"].(map[string]interface{}))["author"] = mod.User.Username
+        (m["mod"].(map[string]interface{}))["game_short"] = mod.Game.Short
     }
 }
